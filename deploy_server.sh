ab7ee487704cb1db893137179b5ebdee9167e9ad
alisalim17 <alisalimli1899@gmail.com>
2021-07-29 19:51:14 +0400

    trying ssh to server

--- deploy_server.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# ssh to my server's ip & pull from docker hub & tag it & deploy it via dokku
-ssh root@164.90.169.15 "docker pull alisalim17/abb:latest && docker tag alisalim17/abb:latest dokku/api:latest && dokku tags:deploy api latest"
-
