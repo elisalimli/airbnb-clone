419adc4e6554877c76190ad0a0882167cb213203
alisalim17 <alisalimli1899@gmail.com>
2021-08-03 19:21:01 +0400

    trying deploy frontend

--- packages/web/src/pages/register.tsx
+++ packages/web/src/pages/register.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import RegisterConnector from "../modules/register/RegisterConnector";
 import UnprotectedRoute from "../ui/utils/UnprotectedRoute";
 import { withApollo } from "../utils/withApollo";
+
 const Register: React.FC = () => {
   return (
     <UnprotectedRoute>
