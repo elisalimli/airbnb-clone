d13ad32eb399c2a18365b777a0936b6f146fdb8c
alisalim17 <alisalimli1899@gmail.com>
2021-08-10 08:33:03 +0400

    fix something

---
+++ packages/web/out_functions/next_register/nextPage/chunks/799.js
@@ -0,0 +1,61 @@
+exports.id = 799;
+exports.ids = [799];
+exports.modules = {
+
+/***/ 75192:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(52322);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(97729);
+
+
+
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+
+
+const MyApp = ({
+  Component,
+  pageProps
+}) => {
+  // createPageProgressBar();
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+    children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(next_head__WEBPACK_IMPORTED_MODULE_1__.default, {
+      children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no,user-scalable=0"
+      })
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, _objectSpread({}, pageProps))]
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (MyApp);
+
+/***/ }),
+
+/***/ 32738:
+/***/ (function(module) {
+
+"use strict";
+module.exports = JSON.parse('{"polyfillFiles":["static/chunks/polyfills-cf519ab7dd9a5a4fd835.js"],"devFiles":[],"ampDevFiles":[],"lowPriorityFiles":["static/DYfjlE_ug9XdviHYi8g0c/_buildManifest.js","static/DYfjlE_ug9XdviHYi8g0c/_ssgManifest.js"],"pages":{"/":["static/chunks/webpack-5b8c845b96f3bbae8650.js","static/chunks/framework-e7644ca443da4d182644.js","static/chunks/main-8485358b76258d54cfd6.js","static/chunks/170-535fe368a6977488ae59.js","static/chunks/pages/index-2f71cec45eddb636b346.js"],"/[testId]":["static/chunks/webpack-5b8c845b96f3bbae8650.js","static/chunks/framework-e7644ca443da4d182644.js","static/chunks/main-8485358b76258d54cfd6.js","static/chunks/pages/[testId]-29e3bf2f40b62c6ca39b.js"],"/_app":["static/chunks/webpack-5b8c845b96f3bbae8650.js","static/chunks/framework-e7644ca443da4d182644.js","static/chunks/main-8485358b76258d54cfd6.js","static/css/47b8a6c68cf6d88dd75a.css","static/chunks/pages/_app-483ead074e33898ed04d.js"],"/_error":["static/chunks/webpack-5b8c845b96f3bbae8650.js","static/chunks/framework-e7644ca443da4d182644.js","static/chunks/main-8485358b76258d54cfd6.js","static/chunks/pages/_error-f0c8671670e02d5c5929.js"],"/register":["static/chunks/webpack-5b8c845b96f3bbae8650.js","static/chunks/framework-e7644ca443da4d182644.js","static/chunks/main-8485358b76258d54cfd6.js","static/chunks/170-535fe368a6977488ae59.js","static/chunks/660-645bc1d5dc80db6e5f22.js","static/chunks/pages/register-7a8336cd98dee2daf27d.js"]},"ampFirstPages":[]}');
+
+/***/ }),
+
+/***/ 19392:
+/***/ (function(module) {
+
+"use strict";
+module.exports = JSON.parse('{"..\\\\..\\\\..\\\\..\\\\node_modules\\\\next-apollo\\\\dist\\\\index.es.js -> @apollo/client/react/ssr":{"id":4030,"files":["static/chunks/framework-e7644ca443da4d182644.js","static/chunks/30.8df1efc40208a4c998c0.js"]}}');
+
+/***/ })
+
+};
+;
