d6ca0491f508dc30b5ce790a298f4557e1186418
alisalim17 <alisalimli1899@gmail.com>
2021-08-10 15:36:04 +0400

    fix tailwind css import

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -2,12 +2,13 @@
 import Head from "next/head";
 import "../styles/globals.css";
 
-if (process.env.NODE_ENV === "production")
-  // @ts-ignore
-  // there is index css and tailwind css file,but typescript gives error
-  import("../styles/index.css");
-// @ts-ignore
-else import("../styles/tailwind.css");
+// if (process.env.NODE_ENV === "production")
+//   // @ts-ignore
+//   // there is index css and tailwind css file,but typescript gives error
+//   import("../styles/index.css");
+// // @ts-ignore
+// else
+import "../styles/tailwind.css";
 
 const MyApp = ({ Component, pageProps }) => {
   // createPageProgressBar();
