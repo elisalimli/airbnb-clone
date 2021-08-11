0c04854e492365c8602ec99f960e6ac32cc30de5
alisalim17 <alisalimli1899@gmail.com>
2021-08-11 15:34:32 +0400

    change server ip in deploy_server.sh

--- deploy_server.sh
+++ deploy_server.sh
@@ -13,4 +13,4 @@ docker build -t alisalim17/abb:latest .
 docker push alisalim17/abb:latest
 
 # ssh to my server's ip & pull from docker hub & tag it & deploy it via dokku
-ssh root@68.183.214.220 "docker pull alisalim17/abb:latest && docker tag alisalim17/abb:latest dokku/api:latest && dokku tags:deploy api latest"
+ssh root@167.71.64.47 "docker pull alisalim17/abb:latest && docker tag alisalim17/abb:latest dokku/api:latest && dokku tags:deploy api latest"
