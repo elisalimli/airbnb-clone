cc02771fb4ff5611d034b7c7bbc9d6fb69546d1e
alisalim17 <alisalimli1899@gmail.com>
2021-08-23 08:10:22 +0400

    testing new deploy

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { withApollo } from "../utils/withApollo";
 
 const Home = () => {
-  return <div>username </div>;
+  return <div>username 2 </div>;
 };
 
 export default withApollo({ ssr: true })(Home);
