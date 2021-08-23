f121bcfd1c5b034ee5dfdc7473982bf3ee9eda90
alisalim17 <alisalimli1899@gmail.com>
2021-08-23 13:56:08 +0400

    build css

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { withApollo } from "../utils/withApollo";
 
 const Home = () => {
-  return <div>username 2 </div>;
+  return <div>Hello world</div>;
 };
 
 export default withApollo({ ssr: true })(Home);
